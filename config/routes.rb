b096229f5cad34e0de9c763f1ec7b38ffce2db1a
mile <milenajohanna1@gmail.com>
2022-03-01 16:15:28 -0300

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   root to: 'pages#home'
-    resources :users, only: [:index, :show, :new] do
+    resources :users, only: [:index, :show, :new, :edit, :update] do
        resources :requests, only: [:create]
     end
     resources :requests, only: [:index, :update] do
