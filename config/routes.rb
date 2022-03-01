807e9ad3e5512ddd5f669be5d202a707f961fa92
Marines Ponce de Leon <marines.poncedeleon@gmail.com>
2022-03-01 10:54:06 -0500

    update gemfile.lock and request route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-    resources :users, only: [:index, :show, :new] do
-       resources :requests, only: [:create]
-    end
-    resources :requests, only: [:index, :update] do
-      resources :reviews, only: :create
-    end
+  resources :users, only: [:index, :show, :new] do
+    resources :requests, only: [:new, :create]
+  end
+  resources :requests, only: [:index, :update] do
+    resources :reviews, only: :create
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
