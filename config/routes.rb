317efa3adc45d65fdc134688c8505fa3baf25893
Emanuel Chareun <emachareun@gmail.com>
2022-03-01 12:54:19 -0300

    agregamos rutas

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
        resources :requests, only: [:create]
     end
     resources :requests, only: [:index, :update] do
-      resources :reviews, only: :create
+      resources :reviews, only: [:create]
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
