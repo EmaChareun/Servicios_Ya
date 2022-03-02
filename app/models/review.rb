5289cc56fcc61565f6fb51f3fd405a4a166ee9b1
Emanuel Chareun <emachareun@gmail.com>
2022-03-02 13:52:02 -0300

    controlador reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :request
   belongs_to :user
+  validates :content, presence: true
+  validates :rating, numericality: true, inclusion: { in: (1..5) }
 end
