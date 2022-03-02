0ea73fd45feeed24887aa92a9ff9dda48f08b27b
mile <milenajohanna1@gmail.com>
2022-03-02 12:08:19 -0300

    pude discriminar para que index solo muestre a los profesionales

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
-    @users = User.all
+    #@users = User.all
+    @users_profesionals = User.where(role: "profesional")
     # @users = policy_scope(User)
   end
 
