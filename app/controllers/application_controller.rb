04d456c8395bb0a0b82c5c0711d794941049fb57
mile <milenajohanna1@gmail.com>
2022-03-02 17:19:40 -0300

    cambios en login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # include Pundit
 
   # Pundit: white-list approach.
@@ -13,7 +15,15 @@ class ApplicationController < ActionController::Base
   # #   redirect_to(root_path)
   # # end
 
-  # private
+  private
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :job, :phone_number, :role, :avatar])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :city, :job, :phone_number, :role, :avatar])
+  end
 
   # def skip_pundit?
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
