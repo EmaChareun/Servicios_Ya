3bff032d3d7d48ef6614040248e3e311af22e7c5
Emanuel Chareun <emachareun@gmail.com>
2022-03-03 10:04:29 -0300

    mejoras en el requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_160123) do
+ActiveRecord::Schema.define(version: 2022_03_02_214643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_03_02_160123) do
     t.bigint "professional_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "comment"
+    t.integer "balance"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
