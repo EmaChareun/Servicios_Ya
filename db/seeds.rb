06f1701f61a77279a8b121219c121b135a4f4163
Emanuel Chareun <emachareun@gmail.com>
2022-03-04 16:54:14 -0300

    seed 35

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+lista_oficio = ["A/A", "Albañil",	"Arquitecto",	"Autos",	"Belleza",	"Bienestar",	"Carpintero",	"Cerrajero", "Colocador", "Control de plagas", "Cuidador",	"Decorador", "Electricista",	"Gasista",	"Herrero",	"Jardinero", "Limpieza",	"Mascotas", "Mudanzas", "Piletas", "Pintor",	"Plomero", "Reformas", "Seguridad", "Tapicero", "Técnico"]
+
 10.times do |i|
   User.create(email: "client#{i}@example.com", password: "111111", password_confirmation: "111111", first_name: "cliente#{i}", last_name: "elapellido#{i}", city: "Buenos Aires", phone_number: "12345678" , role: "cliente", job: ["clientes"], avatar: "")
 end
@@ -15,5 +17,3 @@ end
 10.times do |i|
   User.create(email: "profesional#{i}@example.com", password: "111111", password_confirmation: "111111", first_name: "profesional#{i}", last_name:  "elapellido#{i}", city: "Buenos Aires", phone_number: "12344566778", role: "profesional", job: lista_oficio.sample(3), validation: random_boolean = [true, false].sample, avatar: "")
 end
-
-lista_oficio = ["A/A", "Albañil",	"Arquitecto",	"Autos",	"Belleza",	"Bienestar",	"Carpintero",	"Cerrajero", "Colocador", "Control de plagas", "Cuidador",	"Decorador", "Electricista",	"Gasista",	"Herrero",	"Jardinero", "Limpieza",	"Mascotas", "Mudanzas", "Piletas", "Pintor",	"Plomero", "Reformas", "Seguridad", "Tapicero", "Técnico"]
