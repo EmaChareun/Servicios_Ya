9d34e413b0c229435ed4a8be7a59d39e95449c3f
Emanuel Chareun <emachareun@gmail.com>
2022-03-04 16:51:03 -0300

    seed 34

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 10.times do |i|
-  User.create(email: "client#{i}@example.com", password: "111111", password_confirmation: "111111", first_name: "cliente#{i}", last_name: "elapellido#{i}", city: "Buenos Aires", phone_number: "12345678" , role: "cliente", job: ["clientes"], avatar: "", size: "50x50", format: "jpg"))
+  User.create(email: "client#{i}@example.com", password: "111111", password_confirmation: "111111", first_name: "cliente#{i}", last_name: "elapellido#{i}", city: "Buenos Aires", phone_number: "12345678" , role: "cliente", job: ["clientes"], avatar: "")
 end
 
 10.times do |i|
-  User.create(email: "profesional#{i}@example.com", password: "111111", password_confirmation: "111111", first_name: "profesional#{i}", last_name:  "elapellido#{i}", city: "Buenos Aires", phone_number: "12344566778", role: "profesional", job: lista_oficio.sample(3), validation: random_boolean = [true, false].sample, avatar: "", size: "50x50", format: "jpg"))
+  User.create(email: "profesional#{i}@example.com", password: "111111", password_confirmation: "111111", first_name: "profesional#{i}", last_name:  "elapellido#{i}", city: "Buenos Aires", phone_number: "12344566778", role: "profesional", job: lista_oficio.sample(3), validation: random_boolean = [true, false].sample, avatar: "")
 end
 
 lista_oficio = ["A/A", "Albañil",	"Arquitecto",	"Autos",	"Belleza",	"Bienestar",	"Carpintero",	"Cerrajero", "Colocador", "Control de plagas", "Cuidador",	"Decorador", "Electricista",	"Gasista",	"Herrero",	"Jardinero", "Limpieza",	"Mascotas", "Mudanzas", "Piletas", "Pintor",	"Plomero", "Reformas", "Seguridad", "Tapicero", "Técnico"]
