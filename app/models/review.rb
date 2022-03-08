fc61fe401469de5b723a4fe5c22896f17911b82e
Emanuel Chareun <emachareun@gmail.com>
2022-03-08 11:22:53 -0300

    seed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   validates :request_id, uniqueness: { scope: :user_id, message: "Ya hiciste una reseña" }
-  belongs_to :request, dependent: :destroy
-  belongs_to :user, dependent: :destroy
+  belongs_to :request,
+  belongs_to :user,
   validates :content, presence: true
   validates :rating, numericality: true, inclusion: { in: (1..5) }
 end
