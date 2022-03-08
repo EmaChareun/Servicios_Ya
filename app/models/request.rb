33377a4359f74e742b3574fcdc5aa472465277d0
Emanuel Chareun <emachareun@gmail.com>
2022-03-08 11:33:00 -0300

    seed

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,9 +1,9 @@
 class Request < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   belongs_to :professional, class_name: "User", foreign_key: "professional_id", optional: true
-  has_many :reviews
+  # when request is destroyed, destroy will be called on its associated reviews.
+  has_many :reviews, dependent: :destroy
   has_many_attached :photos
-
   validates :title, presence: true
   validates :detail, presence: true
   validates :city, presence: true
