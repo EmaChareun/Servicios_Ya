c4069997f963562d412c4967218f752829443148
Emanuel Chareun <emachareun@gmail.com>
2022-03-08 11:06:06 -0300

    seed actualizadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 require 'faker'
+puts "Destruyendo seeds"
+Review.destroy_all
+Request.destroy_all
+User.destroy_all
 
+puts "Creando seeds"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,15 +13,20 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 lista_oficio = ["A/A", "Albañil",	"Arquitecto",	"Autos",	"Belleza",	"Bienestar",	"Carpintero",	"Cerrajero", "Colocador", "Control de plagas", "Cuidador",	"Decorador", "Electricista",	"Gasista",	"Herrero",	"Jardinero", "Limpieza",	"Mascotas", "Mudanzas", "Piletas", "Pintor",	"Plomero", "Reformas", "Seguridad", "Tapicero", "Técnico"]
 
+lista_ciudades = ["Buenos Aires", "Tigre", "San Fernando", "Don Torcuato", "El Talar", "Tortuguitas"]
+
+lista_nombres = ["Marcos", "Florencia", "Marines", "Juan", "Esteban", "Santiago"]
+
+lista_apellidos = ["Sanchez", "Avila", "Viera", "Chareun", "Ponce de Leon", "Garcia", "Perez"]
+
 10.times do |i|
-  User.create!(email: Faker::Internet.email, password: "111111", password_confirmation: "111111", first_name: "cliente#{i}", last_name: Faker::Name.last_name, city: Faker::Address.city, phone_number: Faker::PhoneNumber.cell_phone, role: "cliente", job: ["clientes"], avatar: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"))
+  User.create!(email: Faker::Internet.email, password: "111111", password_confirmation: "111111", first_name: lista_nombres.sample, last_name: lista_apellidos.sample, city: lista_ciudades.sample, phone_number: Faker::PhoneNumber.cell_phone, role: "cliente", job: ["clientes"], avatar: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"))
 end
 
 10.times do |i|
-  User.create!(email: Faker::Internet.email, password: "111111", password_confirmation: "111111", first_name: "profesional#{i}", last_name: Faker::Name.last_name, city: "Santiago", phone_number: Faker::PhoneNumber.cell_phone, role: "profesional", job: lista_oficio.sample(3), validation: random_boolean = [true, false].sample, avatar: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"))
+  User.create!(email: Faker::Internet.email, password: "111111", password_confirmation: "111111", first_name: lista_nombres.sample, last_name: lista_apellidos.sample, city: lista_ciudades.sample, phone_number: Faker::PhoneNumber.cell_phone, role: "profesional", job: lista_oficio.sample(3), validation: random_boolean = [true, false].sample, avatar: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"))
 end
 
-puts "Seeds Creadas"
+puts "Seeds creadas!"
